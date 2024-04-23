2100e43347c96ff35c70cdcd40351704af8bc2da
Flazer0136 <Blu0136@gmail.com>
2024-04-22 22:40:47 -0600

    Fixed all erros and media queries

--- Assignment-3/JavaScript-Files/index.js
+++ Assignment-3/JavaScript-Files/index.js
@@ -1,3 +1,33 @@
+function userSubmitMax0() {
+    var x = document.forms["voteForm0"]["name0"].value;
+    if (x == "") {
+        alert("Please enter a name");
+    }
+    else {
+        document.getElementById("voteForm0").style.display = 'none';
+        document.getElementById("pollResultsMax0").style.display = 'block';
+    }
+}
+function userSubmitTen0() {
+    var x = document.forms["voteForm0"]["name0"].value;
+    if (x == "") {
+        alert("Please enter a name");
+    }
+    else {
+    document.getElementById("voteForm0").style.display = 'none';
+    document.getElementById("pollResultsTen0").style.display = 'block';
+    }
+}
+function userSubmitMona0() {
+    var x = document.forms["voteForm0"]["name0"].value;
+    if (x == "") {
+        alert("Please enter a name");
+    }
+    else {
+    document.getElementById("voteForm0").style.display = 'none';
+    document.getElementById("pollResultsMona0").style.display = 'block';
+    }
+}
 function userSubmitMax() {
     var x = document.forms["voteForm"]["name"].value;
     if (x == "") {
